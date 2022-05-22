41bf8d83d36b157d71bafe5242872af61d42f448
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2022-05-22 15:47:03 +0300

    moved descriptives to other script. performed analyses to check errors.

--- data_prep.R
+++ data_prep.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(readxl)
 
 data_folder = "D:\\phd\\jump load\\data\\"
-
 d_all = read_excel(paste0(data_folder, "Final Data Set_Daily_Weekly_Pre.xlsx"), "FDS_Daily_Pre_All", na = "", skip = 4)
 d_all = d_all %>% tibble()
 
@@ -36,47 +35,11 @@ d_all = d_all %>% mutate(inj_knee = ifelse(Knee_1 >= 1| Knee_2 >= 1 | Knee_3 >= 
                          inj_lowback = ifelse(LowBack_1 >= 1| LowBack_2 >= 1 | LowBack_3 >= 1 | LowBack_4 >= 1, 1, 0),
                          inj_lowback_subst = ifelse(LowBack_2 >= 17 | LowBack_3 >= 17, 1, 0))
 
-# number of knee complaint cases
-d_all %>% summarise(sum(inj_knee == 1, na.rm = TRUE))
-
-# how many players have symptoms?
-d_knee_perplayer = d_all %>% count(PlayerID, inj_knee) 
-
-d_knee_perplayer %>% group_by(PlayerID) %>% 
-  summarise(injured = sum(inj_knee == 1, na.rm = TRUE)) %>% 
-  count(injured)
-
-# number of players who had complaints for more than 1 week
-d_knee_perplayer %>% filter(inj_knee == 1) %>% summarise(sum(n>=2))
-
-n_weeks_perplayer = d_knee_perplayer %>% filter(!is.na(inj_knee)) %>% group_by(PlayerID) %>% summarise(denom = sum(n))
-
-mean_weeks_complaints = d_knee_perplayer %>% 
-  filter(inj_knee == 1) %>% 
-  left_join(n_weeks_perplayer, by = "PlayerID") %>% 
-  mutate(prop = n/denom, mean_prop = mean(prop))
-
-# how many our missing the OSTRC questionnaire?
-d_ostrc_completed = d_all %>% select(starts_with("completed")) %>% filter(!is.na(completed_team_ostrc))
-nrow(d_ostrc_completed)
- 
-# missing on completed variable means that the response was missing
-d_ostrc_completed %>% filter(is.na(completed_today_Knee_OSTRC))
-
-# only questions 2 and 3 pertain to substantial injuries, 
-# and only if they answered reply number 3 or 4
-# our data is structured for severity scores, 
-# and have the options 0, 8, 17, 25, equaling response number 1, 2, 3, or 4
-
-# number of days with jumper's knee
-d_all %>% summarise(sum(inj_knee == 1, na.rm = TRUE))
-
 # write csv to read in other scripts
 # write .csv
 # write_delim is preferable, but write_excel_csv is required for excel to understand
 # that the file encoding is UTF-8
 # write_excel_csv(d_all, paste0(data_folder, "d_volleyball.csv"), delim = ";", na = "")
-
 
 # checking that no jump height sums are unrealistically extreme
 d_jump_height_test = d_all %>% select(Date, PlayerID, jumps_n, jump_height_sum) %>% 
@@ -98,7 +61,7 @@ d_all %>% count(MatchParticipation)
 
 #---------------------------------------------------exposure data
 
-key_cols = c("date", "id_player", "id_team", "id_team_player", "id_season")
+key_cols = c("date", "id_player", "id_team", "id_team_player", "id_season", "session_type")
 d_all = d_all %>% rename(date = Date, 
                          id_team = Team, 
                          season_phase = `Season Phase`,
@@ -115,13 +78,14 @@ d_daily = d_all %>% select(key_cols,
                  year, month_day, season, 
                  season_phase,
                  age,
-                starts_with("Match"))
+                 starts_with("Match"),
+                 session_type)
 
 # column specs for jump data
 jump_level_cols = cols(
   .default = col_double(),
   date = col_date(format = ""),
-  datetime = col_datetime(format = "%Y-%m-%d %H:%M:%OS"),
+  datetime = col_character(),
   match_number = col_character(),
   session_type = col_character(),
   position = col_character(),
@@ -134,7 +98,6 @@ jump_level_cols = cols(
 )
 
 d_jump_all = read_delim(paste0(data_folder,"data_per_jump.csv"), delim = ";", na = "", col_types = jump_level_cols)
-d_jump_all = d_jump_all %>% mutate(session_type = ifelse(is.na(session_type), "no volleyball", session_type))
 d_jump_all = d_jump_all %>% mutate(imputed = ifelse(is.na(imputed), "No", imputed))
 d_jump_all = d_jump_all %>% mutate(jump_height = ifelse(imputed == "Yes", NA, jump_height))
 
@@ -158,67 +121,43 @@ d_all %>% filter(session_type == "match" |
                  session_type == "practice", jumps_n == 0, 
                  MatchParticipation == "Significant Contributor") 
 
+
+dates_matchpractice = d_jump_all %>% filter(session_type == "match" | session_type == "practice" | session_type == "friendly") %>% distinct(date)
+
+dates_matchpractice_daily = d_daily %>% filter(session_type == "match" | session_type == "practice" | session_type == "friendly") %>% distinct(date)
+
+setdiff(dates_matchpractice, dates_matchpractice_daily)
+
+dates = d_jump_all  %>% distinct(date)
+dates_daily = d_daily %>% distinct(date)
+diffdates = setdiff(dates_daily, dates)
+
+d_daily %>% filter(date %in% diffdates$date) %>% count(session_type)
+
+
+
+d_jump_all %>% filter(date == "2018-09-21")
+d_jump_all %>% filter(date == "2018-09-15") %>% View()
+
+d_jump_all %>% filter(date == "2017-09-04") %>% View()
+d_daily %>% filter(date == "2017-09-04") %>% View()
+
+d_jump_all %>% select(key_cols)
+d_daily %>% select(key_cols, inj_knee)
+
 # join jump-level data with daily and weekly data
-d_jump = d_jump_all %>% left_join(d_daily, by = key_cols) %>% select(-match_number)
+d_jump = d_jump_all %>% full_join(d_daily, by = key_cols) %>% select(-match_number)
+
+d_jump %>% filter(date == "2017-09-10", id_player == 1, id_team_player == "A-1-1", id_season == "A-1", id_team == "A")
+
+d_jump %>% filter(date == "2017-09-10") %>% select(session_type)
+
+d_jump %>% View()
+
+
+d_jump_all %>% filter(date == "2017-09-04")
+
 
 write_excel_csv(d_jump, 
                 "d_jump.csv", 
                 delim = ";", na = "")
-
-#---------------------------------------------------trends--------------------------------------
-
-
-# trends. Does there seem to be a season effect?
-# only looking at days that players play volleyball (match or training)
-# see if there is any pattern in the injuries
-d_mean_day = d_all %>% filter(SessionType != "no volleyball") %>% 
-              group_by(Date, year, season, month_day) %>% summarise(mean_jumps = mean(jumps_n), 
-                                           mean_height = mean(jump_height_sum, na.rm = TRUE),
-                                           sum_injuries = sum(inj_knee, na.rm = TRUE)) %>% ungroup()
-
-ggplot(d_mean_day, aes(x = month_day, y = mean_jumps, group = 1)) +
-  facet_wrap(~year) +
-  geom_line(size = 1) +
-  geom_point(aes(x = month_day, y = sum_injuries))
-
-ggplot(d_mean_day, aes(x = Date, y = mean_height)) +
-  geom_line()
-
-# do matches have higher/more frequent than other training?
-d_mean_day_match = d_all %>% 
-                    filter(SessionType != "no volleyball") %>% group_by(Date, Match) %>% 
-                    summarise(mean_jumps = mean(jumps_n), 
-                              mean_height = mean(jump_height_sum, na.rm = TRUE))
-
-
-ggplot(d_mean_day_match, aes(x = Date, y = mean_jumps, group = Match)) +
-  geom_line()
-
-
-d_mean_match = d_all %>% 
-  filter(SessionType != "no volleyball") %>% group_by(Match) %>% 
-  summarise(mean_jumps = mean(jumps_n), 
-            mean_height = mean(jump_height_sum, na.rm = TRUE)) %>% ungroup()
-
-# fewer and lower jumping during match? 
-ggplot(d_mean_match, aes(y = Match, x = mean_jumps)) +
-  ggstance::geom_barh(stat = "identity", color = "black")
-
-ggplot(d_mean_match, aes(y = Match, x = mean_height)) +
-  ggstance::geom_barh(stat = "identity", color = "black")
-
-# what happens if we remove preseason?
-
-d_mean_match = d_all %>% 
-  filter(SessionType != "no volleyball", Season.Phase != "Preseason") %>% group_by(Match) %>% 
-  summarise(mean_jumps = mean(jumps_n), 
-            mean_height = mean(jump_height_sum, na.rm = TRUE)) %>% ungroup()
-
-# fewer and lower jumping during match?
-# effect not as extreme
-ggplot(d_mean_match, aes(y = Match, x = mean_jumps)) +
-  ggstance::geom_barh(stat = "identity", color = "black")
-
-ggplot(d_mean_match, aes(y = Match, x = mean_height)) +
-  ggstance::geom_barh(stat = "identity", color = "black")
-
