fcdadec0d0dbabb34842f2b79babc5e94abed1b2
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2022-05-24 10:03:51 +0300

    Calculated weekly number of jumps and weekly sum of jumps.

--- analysis.R
+++ analysis.R
@@ -5,9 +5,8 @@ library(slider)
 data_folder = "D:\\phd\\jump load\\data\\"
 
 d_all = read_delim(paste0(data_folder, "d_volleyball.csv"), delim = ";", na = "")
-
+key_cols = c("date", "id_player", "id_team", "id_team_player", "id_season")
 # baseline injuries
-
 
 #------------------------------weekly training load sums-------------------
 
@@ -19,47 +18,20 @@ d_all = read_delim(paste0(data_folder, "d_volleyball.csv"), delim = ";", na = ""
 # compared to past training, also past training in the current week.
 # We can still consider the effects of past daily training load before the current week.
 
-
-d_nested_player = d_all %>% group_by(PlayerID) %>% nest()
-d_nested_player$data %>% map(. %>% slice(1:7) %>% select(Date, inj_knee))
-
-
-# Fetch dates in which the OSTRC was collected
-d_all = d_all %>% mutate(ostrc_day = ifelse(is.na(Knee_Total), 0, 1))
-d_ostrc_dates = d_all %>% group_by(PlayerID) %>% filter(ostrc_day == 1) %>% select(Date, PlayerID)
-
-# every 7th date after the first OSTRC was supposed to be filled
-
-
-example_player_dates = d_ostrc_dates %>% filter(PlayerID == 1)
-
-example_player = d_all %>% filter(PlayerID == 1) %>% select(Date, jumps_n, PlayerID)
-
-
-example_player %>% 
-  mutate(jumps_weekly_n = 
-           slide_dbl(jumps_n, ~sum(.), .before = 6, step = 7, complete = TRUE)) %>% View()
-
-slide_index_dbl(example_player, example_player$Date, sum, .before = lubridate::days(6))
-
-?slide_index_dbl
-slide_period(example_player, example_player$Date, "week", ~.x)
-
-
 # function to calculate sums across a user-specified window size
-slide_sum = function(x, window_size){
-  l = slide(x, ~sum(.), .before = 6, step = window_size, .complete =TRUE)
+slide_sum = function(x, window_size, complete){
+  l = slide(x, ~sum(.), .before = 6, step = window_size, .complete = complete)
   vector = unlist(l)
   v_calc = vector[seq(1, length(vector), window_size)]
   v_calc
 }
 
 # make window object to designated length (7 days)
-window = 7
+window_7 = 7
 
 # First step is to test that our automated function 
 # for calculating weekly sums (slide_sum) calculates correctly. 
-example_player = d_all %>% filter(PlayerID == 1) %>% select(Date, jumps_n, PlayerID, inj_knee)
+example_player = d_all %>% filter(id_player == 1) %>% select(date, jumps_n, id_player, inj_knee)
 
 # manual calculation of first 3 weekly sums
 v_correct = c(
@@ -69,8 +41,8 @@ v_correct = c(
 )
 
 # function above used to calculate weekly sums
-test_loads = (example_player %>% pull(jumps_n))[1:(window*3)]
-v_calc = slide_sum(test_loads, window)
+test_loads = (example_player %>% pull(jumps_n))[1:(window_7*3)]
+v_calc = slide_sum(test_loads, window_7, TRUE)
 
 # test that the output from the function equals the manual calculation
 testthat::expect_equal(v_correct, v_calc)
@@ -78,93 +50,45 @@ testthat::expect_equal(v_correct, v_calc)
 # Next step is to perform the calculation on the data
 # We nest on each player in the data so that weekly sum-calculation
 # does not slide over different players
-nested_list = d_all %>% group_by(PlayerID) %>% nest()
-l_weekly_sums = nested_list$data %>% map(., ~slide_sum(.$jumps_n, window))
+window = 1
+nested_list = d_all %>% group_by(id_player) %>% nest()
+nested_list$data = nested_list$data %>% map(., ~slide_sum(.$jumps_n, window, FALSE))
+d_weekly_load = unnest(nested_list, cols = c(data)) %>% 
+                ungroup() %>% mutate(index = 1:n()) %>% 
+                rename(jumps_n_weekly = data)
 
-example_sums = l_weekly_sums[[1]]
-example_player_dates = d_ostrc_dates %>% filter(PlayerID == 1)
+d_all = d_all %>% mutate(index = 1:n()) %>% left_join(d_weekly_load, by = c("id_player", "index"))
 
-example_player %>% pull(Date)
+# repeat for jump height
+nested_list = d_all %>% group_by(id_player) %>% nest()
+nested_list$data = nested_list$data %>% map(., ~slide_sum(.$jump_height_sum, window, FALSE))
+d_weekly_load = unnest(nested_list, cols = c(data)) %>% 
+  ungroup() %>% mutate(index = 1:n()) %>% 
+  rename(jumps_height_weekly = data)
 
-example_player %>% View()
+d_all = d_all %>% mutate(index = 1:n()) %>% left_join(d_weekly_load, by = c("id_player", "index"))
 
-
-length(example_sums)
-length(example_player_dates$Date)
-
-example_player = d_all %>% filter(PlayerID == 1) %>% select(Date, jumps_n, PlayerID)
-
-
-
-
-# we combine the sums to the original data
-# but we only include the dates where OSTRC was collected
-nested_list$data %>% 
-  map(. %>% filter(Date %in% d_ostrc_dates) %>% select(Date)) %>% 
-  map2(.x = ., .y = l_weekly_sums, ~.x %>% mutate(jumps_n_weekly = .y))
-
-
-slide_sum(example_dude$jumps_n)
-
-
-
-
-
-
-
-l_weekly_sums[[1]]
-
-
-(nested_list$data %>% 
-  map(. %>% filter(Date %in% d_ostrc_dates) %>% select(Date)))[[1]]
-
-
-?map2
-
-nested_list$data = nested_list$data %>% map(., ~slide_sum(.$jumps_n))
-
-
-d_weekly %>% select(Date, jumps_n, jumps_n_weekly, PlayerID) %>% filter(PlayerID == 1, !is.na(jumps_n_weekly))
-
-
-
-d_unnest = unnest(nested_list, cols = c(data)) %>% mutate(index = 1:n()) %>% ungroup()
-
-d_unnest %>% filter(PlayerID == 2)
-
-
-
-
-
-
-
-
-
-d_weekly_load = d_ostrc_dates %>% 
-  left_join(d_unnest, by = c("PlayerID", "index")) %>% 
-  rename(jumps_n_weekly = data) %>% 
-  select(-index)
-d_weekly = d_all %>% left_join(d_weekly_load, by = c("PlayerID", "Date"))
-
-# test that the weekly sums are aligned with the weekly OSTRC questionnaire in the data
-d_weekly %>% select(Date, PlayerID, jumps_n, jumps_n_weekly, inj_knee) %>% filter(!is.na(inj_knee)) %>% filter(is.na(jumps_n_weekly))
-
-
-
-d_weekly %>% select(Date, PlayerID, jumps_n, jumps_n_weekly, inj_knee) %>% filter(PlayerID == 2)
-
-# load data summed per week, but with past load data available
-
-
-# injury data structered in intervals
-d_all %>% select(Date, starts_with("inj"))
+d_analysis = d_all %>% 
+             select(key_cols, 
+                 jumps_n, 
+                 jumps_n_weekly,
+                 jump_height_sum,
+                 jumps_height_weekly,
+                 jump_height_max,
+                 TotalDailyKELoadIndex,
+                 starts_with("inj"), 
+                 year, month_day, season, 
+                 season_phase,
+                 age, position,
+                 Match,
+                 session_type)
 
 # fill OSTRC questionnaires up so that they pertain for a whole week
-d_all = d_all %>% 
-  fill(starts_with("knee"), 
-       starts_with("Shoulder"), 
-       starts_with("LowBack"), 
-       .direction = "up")
+# d_all = d_all %>% 
+#   fill(starts_with("knee"), 
+#        starts_with("Shoulder"), 
+#        starts_with("LowBack"), 
+#        .direction = "up")
 
 
 library(icenReg)
