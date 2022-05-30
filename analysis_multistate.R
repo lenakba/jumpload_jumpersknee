9754fd1bab24f0640527382b68ec8136c058511d
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2022-05-30 10:39:59 +0300

    Fixed issue with missing state changes.

--- analysis_multistate.R
+++ analysis_multistate.R
@@ -39,18 +39,6 @@ d_analysis = d_jumpload %>%
 
 d_analysis = d_analysis %>% mutate_at(vars(starts_with("inj"), starts_with("knee")), ~as.numeric(.))
 
-# define states
-# we can either take this problem through a 
-# regular vs. substantial injury defition
-# or going from one level (no matter what level)
-# to something worse
-# we will consider the "worsening" problem
-statenames = c("asymptomatic", "symptomatic", "worsening", "improvement")
-l_transitions = list(c(2), 
-                     c(1, 3, 4), 
-                     c(1, 4),
-                     c(1, 3))
-
 # We start by filling the values 6 days down.
 # find which days the OSTRC-questionnaire actually pertains to
 # since each answer is for the previous 6 days including the current day
@@ -75,117 +63,126 @@ d_ostrc_dates_valid = d_ostrc_dates_valid %>%
 d_analysis = d_analysis %>% left_join(d_ostrc_dates_valid, 
                             by = c("d_imp", "id_player", "id_team", "id_team_player", "id_season", "date"))
 
-
-d_analysis %>% select(d_imp, id_player, Knee_Total)
-
 # fixme! find better solution for handling missing responses
 # fixme! maybe assume that 1 NA between two OSTRC intervals of 0 is 0?
 d_analysis = d_analysis %>% mutate(knee_total_filled = ifelse(is.na(knee_total_filled), 0, knee_total_filled),
                                    inj_knee_filled = ifelse(is.na(inj_knee_filled), 0, inj_knee_filled))
 
-# Find each interval, so that we can determine, for each,
-# which state they are in (when they are not 0)
-# and give each an ID
+# We will find the change state of each OSTRC questionnaire.
 
-# find the events
-d_events = d_analysis  %>% 
-  group_by(d_imp) %>% 
-  filter(inj_knee_filled == 1) %>% mutate(id_event = 1:n()) %>% 
-  ungroup() %>% select(d_imp, id_player, date, id_event)
+# define states
+# we can either take this problem through a 
+# regular vs. substantial injury defition
+# or going from one level (no matter what level)
+# to something worse
+# we will consider the "worsening" problem
+# we've included improvement, but for statistical power,
+# maybe it should be the same as symptomatic/worse
+statenames = c("asymptomatic", "symptomatic", "worsening", "improvement")
+l_transitions = list(c(2), 
+                     c(1, 3, 4), 
+                     c(1, 4),
+                     c(1, 3))
+transmat = transMat(l_transitions, statenames)
 
-no_inj_players = d_analysis %>% group_by(id_player) %>% summarise(sum = sum(inj_knee_filled)) %>% 
-  ungroup %>% filter(sum == 0) %>% pull(id_player)
+# find positions of change 
+d_kneelevels = d_inj_struct %>% group_by(d_imp, id_player) %>% 
+  mutate(knee_total_filled_lag = lag(knee_total_filled),
+         knee_total_diff = knee_total_filled - knee_total_filled_lag,
+         change = case_when(Knee_Total >= 1 & knee_total_filled_lag == 0 ~ 1,
+                            knee_total_diff < 0 & Knee_Total != 0 ~ 3,
+                            knee_total_diff > 0 & Knee_Total != 0 ~ 2,
+                            Knee_Total == 0 ~ 0)
+         ) %>% ungroup()
 
-# fetch those with injuries
-d_inj_only = d_analysis %>% filter(!id_player %in% no_inj_players)
-ids = d_inj_only %>% distinct(id_player) %>% pull()
+# these too, have to be filled.
+# We start by filling the values 6 days down.
+# find which days the OSTRC-questionnaire actually pertains to
+# since each answer is for the previous 6 days including the current day
+d_ostrc_dates = d_kneelevels %>% select(d_imp, all_of(key_cols), change) %>% filter(!is.na(change))
+d_ostrc_dates = d_ostrc_dates %>% mutate(date_last = date+6)
 
-# function to find event intervals and append them to a dataset
-find_events = function(d, id){
-  d1 = d %>% filter(id_player == id)
-  
-  # find intervals
-  pos_symptoms = which(d1$inj_knee_filled == 1)
-  big_skips = lead(pos_symptoms)-pos_symptoms
-  pos_from = which(big_skips>1)
-  
-  from_after_first = pos_symptoms[pos_from]+1
-  to_after_first = pos_symptoms[pos_from+1]
-  # append to the first extraction
-  from_rows = c(1, from_after_first)
-  to_rows = c(pos_symptoms[1], to_after_first)
-  
-  # if player ends without sympts, needs to be included
-  if((d1 %>% nrow()) > last(pos_symptoms)){
-    from = last(pos_symptoms) + 1
-    end = d1 %>% nrow()
-    from_rows = c(from_rows, from)
-    to_rows = c(to_rows, end)
-  }
-  
-  # slice number of times equal to number of intervals
-  l1mann = replicate(length(from_rows), d1, simplify = FALSE)
-  l_data1person = pmap(list(as.list(from_rows), as.list(to_rows), l1mann), 
-                       function(x, y, z) slice(z, x:y))
-  event_ids = 1:length(l_data1person)
-  l_data1person = map2(l_data1person, event_ids, ~.x %>% mutate(id_event = .y))
-  
-  # collect list of intervals to dataset
-  d_1person = l_data1person %>% bind_rows()
-  d_1person
-}
+nested_list = d_ostrc_dates %>% group_by(d_imp, id_player, id_team, id_team_player, id_season) %>% nest()
+nested_list$data = nested_list$data %>% 
+  map(., ~map2(.x = .$date, .y = .$date_last, .f = ~seq(ymd(.x), ymd(.y), by = "1 day")))
+nested_list$data = nested_list$data %>% map(., ~do.call("c", .))
+d_ostrc_dates_valid = unnest(nested_list, cols = data) %>% ungroup() %>% rename(date = data)
 
-# running above function for each player
-# for each imputed dataset
-datasets = d_inj_only %>% distinct(d_imp) %>% pull() 
-d_maindata = data.frame()
-for(i in datasets){
-  
-  tempdata2 = d_inj_only %>% filter(d_imp == i)  
-  d_1person = data.frame()
-  for(j in ids){
-    tempdata = find_events(tempdata2, j)
-    d_1person = rbind(d_1person, tempdata)
-    d_1person
-  }
-  d_maindata = rbind(d_maindata, d_1person)
-}
+# remove duplicated dates
+# as some OSTRC intervals overlapped
+d_ostrc_dates_valid = d_ostrc_dates_valid %>% 
+  distinct(d_imp, id_player, id_team, id_team_player, id_season, date)
 
-# add on the players without any symptoms
-d_time_to_sympt = bind_rows(d_maindata, d_analysis %>% filter(id_player %in% no_inj_players)) %>% 
-  mutate(id_event = ifelse(id_player %in% no_inj_players, 1, id_event)) %>% 
-  rename(not_unique_id = id_event) 
+d_ostrc_dates_valid = d_ostrc_dates_valid %>% 
+  left_join(d_ostrc_dates, by = c("d_imp", "id_player", "id_team", "id_team_player", "id_season", "date")) %>% 
+  fill(change, .direction = "down") %>% select(-date_last) %>% rename(knee_state = change)
 
-d_eventid = d_time_to_sympt %>% 
-  distinct(id_player, not_unique_id) %>% mutate(id_event = 1:n())
+d_kneelevels = d_kneelevels %>% left_join(d_ostrc_dates_valid, 
+                                      by = c("d_imp", "id_player", "id_team", "id_team_player", "id_season", "date"))
 
 
-d_event_ids = d_time_to_sympt %>% 
-  left_join(d_eventid, by = c("id_player", "not_unique_id")) %>% 
-  select(d_imp, id_player, date, id_event)
 
-d_inj_struct = d_analysis %>% left_join(d_event_ids, by = c("d_imp", "id_player", "date")) %>% 
-             group_by(d_imp, id_player) %>% 
-             fill(id_event, .direction = "down")
+d_kneelevels  %>% 
+  select(d_imp, id_player, Knee_Total, knee_total_filled, knee_state)
 
-d_kneelevels = d_inj_struct %>% group_by(d_imp, id_player, id_event) %>% mutate(knee_total_filled_lag = lag(knee_total_filled),
-                  knee_total_diff = knee_total_filled - knee_total_filled_lag,
-                  knee_state = case_when(Knee_Total >= 1 & knee_total_filled_lag == 0 ~ 1,
-                                         knee_total_diff < 0 & Knee_Total != 0 ~ 3,
-                                         knee_total_diff > 0 & Knee_Total != 0 ~ 2,
-                                         Knee_Total == 0 ~ 0)) %>% ungroup() 
+
+
+
+
+d_selected %>% group_by(d_imp, id_player) %>% 
+  mutate(knee_total_filled_lag = lag(knee_total_filled),
+         knee_total_diff = knee_total_filled - knee_total_filled_lag) %>% View()
+
+
+
+
+d_selected %>% View()
+
+library(msm)
+# add number of days per individual
+d_kneelevels %>% arrange(d_imp, id_player, date) %>%
+  group_by(d_imp, id_player) %>%
+  mutate(day = 1:n()) %>% ungroup() %>% 
+  select(d_imp, id_player, id_event, day, knee_state, jumps_n) %>% View()
+
+
+
+
+
 
 # put states into their own variables
-d_u19_states_temp = d_u19 %>% mutate(knee_mild = ifelse(inj_knee_filled == 1 & overuse == 2, 1, 0),
-                                     acute = ifelse(injury == 1 & substantial == 0 & overuse == 1, 1, 0),
-                                     acute_subst = ifelse(injury == 1 & substantial == 1 & overuse == 1, 1, 0)) %>% 
-  select(p_id, date_training, load, overuse, acute, acute_subst, injury)
+d_kneelevels = d_kneelevels %>% mutate(asymptomatic = ifelse(knee_state == 0, 1, 0),
+                                     symptomatic = ifelse(knee_state == 1, 1, 0),
+                                     worse = ifelse(knee_state == 2, 1, 0),
+                                     better = ifelse(knee_state == 3, 1, 0))
 
 
 
-d_time_to_sympt = d_time_to_sympt %>% arrange(d_imp, id_player, date) %>%
+
+
+
+# add number of days of follow up per event
+d_kneelevels = d_kneelevels %>% arrange(d_imp, id_player, date) %>%
   group_by(d_imp, id_player, id_event) %>%
   mutate(Fup = n(), day = 1:n()) %>% ungroup()
+
+msprep(data = d_kneelevels, trans = tmat, 
+       time = c(NA, "rec", "ae","recae", "rel", "srv"), 
+       status = c(NA, "rec.s", "ae.s", "recae.s", "rel.s", "srv.s"), 
+       keep = c("match", "proph", "year", "agecl"))
+
+msprep(data = d_kneelevels, trans = transmat, 
+       time = c(NA, rep("day", 4)), 
+       status = c(NA, "asymptomatic", "symptomatic", "worse", "better"), 
+       keep = c(key_cols, "jumps_n"))
+
+
+n_trans = max(transmat, na.rm = TRUE)
+trans_vec = 1:n_trans
+
+trans_vec %>% map(.x = ., ~flexsurvreg(Surv(enter, exit, event) ~ load,
+                                       data = subset(d_u19_cpform, event == .x),
+                                       dist = "weibull"))
 
 #--------------------------------Try again using msprep instead of SurvSplit
 
