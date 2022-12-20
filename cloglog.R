c2d0100e40653d9dad9c6ca7ed9e6108f97218ef
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2022-12-20 11:51:09 +0100

    Removed season from predictions.

--- cloglog.R
+++ cloglog.R
@@ -294,7 +294,7 @@ library(ggeffects)
 library(sjPlot)
 preds_jumph = ggpredict(
   fit1, "jumps_height_weekly [all]", 
-  condition = c(age = 26.1, position = "Outside", id_player = "1", season = "2019/2020", week = 3),
+  condition = c(age = 26.1, position = "Outside", id_player = "1", week = 3),
   vcov.fun = "vcovCR", 
   vcov.type = "CR0", 
   vcov.args = list(id_player = unique((d_weekly_dist %>% filter(d_imp == 1))$id_player)),
@@ -302,7 +302,7 @@ preds_jumph = ggpredict(
 
 preds_week = ggpredict(
   fit1, "week [all]", 
-  condition = c(age = 26.1, position = "Outside", id_player = "1", season = "2019/2020", jumps_height_weekly = 2724),
+  condition = c(age = 26.1, position = "Outside", id_player = "1", jumps_height_weekly = 2724),
   vcov.fun = "vcovCR", 
   vcov.type = "CR0", 
   vcov.args = list(id_player = unique((d_weekly_dist %>% filter(d_imp == 1))$id_player)),
@@ -328,7 +328,7 @@ plot_load = ggplot(preds_jumph, aes(x = x, y = predicted, group = 1)) +
   ostrc_theme +
   xlab("Weekly jump load (arb. u)") +
   ylab("Probability of symptoms") +
-  scale_y_continuous(labels = axis_percent)
+  scale_y_continuous(labels = axis_percent, limits = c(0, 0.07))
 
 plot_weeks = ggplot(preds_week, aes(x = x, y = predicted, group = 1)) + 
   geom_area(data = d_dense_week, alpha = 0.3, fill = nih_distinct[1]) +
@@ -337,7 +337,7 @@ plot_weeks = ggplot(preds_week, aes(x = x, y = predicted, group = 1)) +
   ostrc_theme +
   xlab("Number of weeks without symptoms") +
   ylab("Probability of symptoms") +
-  scale_y_continuous(labels = axis_percent)
+  scale_y_continuous(labels = axis_percent, limits = c(0, 0.07))
 
 library(devEMF)
 emf("figure1_predicted_probs.emf", height = 4, width = 12)
